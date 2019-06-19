a60baf71516d6e2233f37b5e5708aa1de8eb6cae
keita_0904 <smaoonn@gmail.com>
2019-06-20 01:00:28 +0900

    リクエストパラム追加

--- src/main/java/com/keita/katayama/githubfunction/controller/GitHubController.java
+++ src/main/java/com/keita/katayama/githubfunction/controller/GitHubController.java
@@ -2,10 +2,7 @@ package com.keita.katayama.githubfunction.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -16,7 +13,10 @@ public class GitHubController {
     private static final String key = "Ke1T@o904S";
 
     @PostMapping
-    public void postFunction(@RequestBody String json) throws IOException {
+    public void postFunction(
+            @RequestBody String json,
+            @RequestParam String url,
+            @RequestParam String token) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         JsonNode node = mapper.readTree(json);
 
